db7f63081e2e343ee273f3a5abe436ef4f9f9921
zb <8761804@iesplayamar.es>
2026-03-01 18:01:28 -0300

    final, falta email

--- js/contact.js
+++ js/contact.js
@@ -35,23 +35,23 @@ $(document).ready(function(){
             },
             messages: {
                 name: {
-                    required: "come on, you have a name, don't you?",
-                    minlength: "your name must consist of at least 2 characters"
+                    required: "Please confirm your name.",
+                    minlength: "Your name must consist of at least 2 characters."
                 },
                 subject: {
-                    required: "come on, you have a subject, don't you?",
-                    minlength: "your subject must consist of at least 4 characters"
+                    required: "Please fill the subject.",
+                    minlength: "Your subject must consist of at least 4 characters."
                 },
                 number: {
-                    required: "come on, you have a number, don't you?",
-                    minlength: "your Number must consist of at least 5 characters"
+                    required: "Please confirm your phone number.",
+                    minlength: "Your number must consist of at least 5 characters."
                 },
                 email: {
-                    required: "no email, no message"
+                    required: "Please confirm your email."
                 },
                 message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
+                    required: "You have to write something to send this form.",
+                    minlength: "Minimum required."
                 }
             },
             submitHandler: function(form) {
